248bf910c5c7fb600525b7b5d7dd0f2e9e678750
ajdb2799 <ajdb2799@gmail.com>
2025-01-29 11:16:02 -0600

    Update script.js

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ document.getElementById('siBtn').addEventListener('click', function() {
 
     // Mostrar el mensaje específico
     document.getElementById('messageContainer').style.display = 'block';
-    document.getElementById('messageContainer').innerHTML = '¡Oh Siii! jajaja';
+    document.getElementById('messageContainer').innerHTML = '¡TIIIIIIIIIII! jajaja';
 
     // Mostrar otro gif después de 3 segundos
     setTimeout(function() {
